b8b86224681ec459f9988a86743915cf8fd4408b
senabalbinar <77548232+senabalbinar@users.noreply.github.com>
2021-02-01 16:52:14 +0300

    Add project files.

---
+++ Business/Concrete/InProductDal.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    internal class InProductDal
+    {
+        internal List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
